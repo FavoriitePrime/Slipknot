3e617016a6bd422ddcdc35d0c283ff132859676c
FavoriitePrime <81848025+FavoriitePrime@users.noreply.github.com>
2023-02-08 16:57:43 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"63e3aa0df0019100228f2a18",
+				"63e3aa0df0019100228f2a1b"
+			]
+		},
+		"63e3aa0df0019100228f2a18": {
+			"id": "63e3aa0df0019100228f2a18",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"63e3aa0df0019100228f2a1b": {
+			"id": "63e3aa0df0019100228f2a1b",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
